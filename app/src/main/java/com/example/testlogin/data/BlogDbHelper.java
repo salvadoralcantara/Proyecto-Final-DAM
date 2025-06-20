d49f42bc9fa39e732ef9d51963ac460e76041571
Guill2 <134907865+Guill2@users.noreply.github.com>
2025-06-19 20:02:05 -0600

    Actualización de creación de posts

--- app/src/main/java/com/example/testlogin/data/BlogDbHelper.java
+++ app/src/main/java/com/example/testlogin/data/BlogDbHelper.java
@@ -13,17 +13,24 @@ public class BlogDbHelper extends SQLiteOpenHelper {
     public static final String C_ID = "_id";
     public static final String C_TITLE = "title";
     public static final String C_STORY = "story";
+    public static final String C_IMAGE_URI = "image_uri";
 
     private static final String CREATE =
             "CREATE TABLE " + TABLE + " (" +
                     C_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
                     C_TITLE + " TEXT NOT NULL, " +
-                    C_STORY + " TEXT NOT NULL );";
+                    C_STORY + " TEXT NOT NULL, " +
+                    C_IMAGE_URI + " TEXT);";
 
     public BlogDbHelper(Context ctx) {
         super(ctx, DB_NAME, null, DB_VERSION);
     }
 
-    @Override public void onCreate(SQLiteDatabase db) { db.execSQL(CREATE); }
-    @Override public void onUpgrade(SQLiteDatabase db,int o,int n){ /* no-op */ }
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(CREATE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) { }
 }
--- app/src/main/java/com/example/testlogin/data/BlogRepository.java
+++ app/src/main/java/com/example/testlogin/data/BlogRepository.java
@@ -5,8 +5,6 @@ import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 
-import com.example.testlogin.model.Blog;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,26 +12,29 @@ public class BlogRepository {
 
     private final BlogDbHelper helper;
 
-    public BlogRepository(Context ctx) { helper = new BlogDbHelper(ctx); }
+    public BlogRepository(Context ctx) {
+        helper = new BlogDbHelper(ctx);
+    }
 
     public long insert(Blog b) {
         SQLiteDatabase db = helper.getWritableDatabase();
         ContentValues cv = new ContentValues();
-        cv.put(BlogDbHelper.C_TITLE, b.title);
-        cv.put(BlogDbHelper.C_STORY, b.story);
+        cv.put(BlogDbHelper.C_TITLE, b.titulo);
+        cv.put(BlogDbHelper.C_STORY, b.historia);
+        cv.put(BlogDbHelper.C_IMAGE_URI, b.imagenUri);
         return db.insert(BlogDbHelper.TABLE, null, cv);
     }
 
     public List<Blog> getAll() {
         SQLiteDatabase db = helper.getReadableDatabase();
-        Cursor c = db.query(BlogDbHelper.TABLE, null, null, null,
-                null, null, BlogDbHelper.C_ID + " DESC");
+        Cursor c = db.query(BlogDbHelper.TABLE, null, null, null, null, null, BlogDbHelper.C_ID + " DESC");
         List<Blog> list = new ArrayList<>();
         while (c.moveToNext()) {
             list.add(new Blog(
                     c.getLong(c.getColumnIndexOrThrow(BlogDbHelper.C_ID)),
                     c.getString(c.getColumnIndexOrThrow(BlogDbHelper.C_TITLE)),
-                    c.getString(c.getColumnIndexOrThrow(BlogDbHelper.C_STORY))
+                    c.getString(c.getColumnIndexOrThrow(BlogDbHelper.C_STORY)),
+                    c.getString(c.getColumnIndexOrThrow(BlogDbHelper.C_IMAGE_URI))
             ));
         }
         c.close();
@@ -43,15 +44,31 @@ public class BlogRepository {
     public void update(Blog b) {
         SQLiteDatabase db = helper.getWritableDatabase();
         ContentValues cv = new ContentValues();
-        cv.put(BlogDbHelper.C_TITLE, b.title);
-        cv.put(BlogDbHelper.C_STORY, b.story);
-        db.update(BlogDbHelper.TABLE, cv, BlogDbHelper.C_ID + "=?",
-                new String[]{String.valueOf(b.id)});
+        cv.put(BlogDbHelper.C_TITLE, b.titulo);
+        cv.put(BlogDbHelper.C_STORY, b.historia);
+        cv.put(BlogDbHelper.C_IMAGE_URI, b.imagenUri);
+        db.update(BlogDbHelper.TABLE, cv, BlogDbHelper.C_ID + "=?", new String[]{String.valueOf(b.id)});
     }
 
     public void delete(long id) {
-        helper.getWritableDatabase()
-                .delete(BlogDbHelper.TABLE, BlogDbHelper.C_ID + "=?",
-                        new String[]{String.valueOf(id)});
+        helper.getWritableDatabase().delete(BlogDbHelper.TABLE, BlogDbHelper.C_ID + "=?", new String[]{String.valueOf(id)});
+    }
+
+    public static class Blog {
+        public long id;
+        public String titulo;
+        public String historia;
+        public String imagenUri;
+
+        public Blog(long id, String titulo, String historia, String imagenUri) {
+            this.id = id;
+            this.titulo = titulo;
+            this.historia = historia;
+            this.imagenUri = imagenUri;
+        }
+
+        public Blog(String titulo, String historia, String imagenUri) {
+            this(-1, titulo, historia, imagenUri);
+        }
     }
 }
